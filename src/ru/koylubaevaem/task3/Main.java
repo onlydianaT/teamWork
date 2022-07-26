54dee6dbc1d802115164032b644bf7c00ccb7af2
Elza Koylubaeva <zakirova_elza@mail.ru>
2022-07-26 19:25:52 +0500

    Added method isPromo

--- src/ru/koylubaevaem/task3/Main.java
+++ src/ru/koylubaevaem/task3/Main.java
@@ -6,30 +6,44 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-//        1.Создание массива продуктов внутри программы (без пользовательского ввода);
+
+        // 1. Создание массива продуктов внутри программы (без пользовательского ввода);
+        String meat = "meat, (1kg)";
+        String potato = "potato, (1kg)";
+        String buckwheat = "buckwheat, (1kg)";
         String[] products = {
-                "meat, (1kg)",
+                meat,
                 "milk, (1l)",
                 "loaf of bread",
-
                 "tea, (0.1kg)",
                 "sugar, (1kg)",
-                "potato, (1kg)",
-                "buckwheat, (1kg)"
+                potato,
+                buckwheat
         };
-//        2.Создание массива цен на продукты (без пользовательского ввода);
+
+        // 2. Создание массива цен на продукты (без пользовательского ввода);
         int[] prices = {427, 73, 42, 90, 78, 63, 129};
         int[] quantity = new int[7];
-        // 3.Вывод списка доступных для покупки продуктов на экран;
+
+        // В программе теперь появится новый массив из названий товаров, которые продаются
+        // по принципу “3 по цене 2х”. Таким образом, за каждые три штуки товара из массива
+        // в корзине цена должна начисляться как за два.
+        String[] promoProducts = { meat, potato, buckwheat };
+
+        // 3. Вывод списка доступных для покупки продуктов на экран;
         for (int i = 0; i < products.length; i++) {
             System.out.println((i + 1) + ": " + products[i] + " " + prices[i] + " rub.");
         }
 
-        int totalSum = 0;
-        int itemOfProduct = 0;
-        int amountOfProduct = 0;
+        System.out.println("Promotional items, 3 for the price of 2: ");
+        for (int i = 0; i < promoProducts.length; i++) {
+            System.out.println((i + 1) + ": " + promoProducts[i]);
+        }
+
+        int itemOfProduct;
+        int amountOfProduct;
         while (true) {
-//            4.Возможность ввода пользователем одной строкой номера продукта и количества для добавления в корзину;
+            // 4. Возможность ввода пользователем одной строкой номера продукта и количества для добавления в корзину;
             System.out.println("Choose the item and amount or 'end' to quit: ");
 
             String inputStr = scan.nextLine();
@@ -59,18 +73,38 @@ public class Main {
                 System.out.println("There is no item with this number.");
                 continue;
             }
-//            5.Пользователь может добавлять несколько раз один и тот же товар в корзину, в этом случае он должен суммировать
+            // 5. Пользователь может добавлять несколько раз один и тот же товар в корзину, в этом случае он должен суммировать
             quantity[itemOfProduct] += amountOfProduct;
-            totalSum += amountOfProduct * prices[itemOfProduct];
 
         }
-//        6.Вывод всех покупок, их общую стоимость и количество, на экран после ввода всех покупок.
+        // 6. Вывод всех покупок, их общую стоимость и количество, на экран после ввода всех покупок.
+        int totalSum = 0;
         for (int i = 0; i < quantity.length; i++) {
             if (quantity[i] != 0) {
-                System.out.println(products[i] + " " + quantity[i] + " items " + prices[i] + " rub/item, " + (quantity[i] * prices[i]) + " rub " + " totally");
+
+                // Проверяем акционный товар или нет
+                String nameOfGood = products[i];
+                boolean promo = isPromo(promoProducts, nameOfGood);
+                int sum;
+                if (promo) {
+                    sum = ((quantity[i] / 3) * 2 + (quantity[i] % 3)) * prices[i];
+                } else {
+                    sum = quantity[i] * prices[i];
+                }
+                System.out.println(products[i] + " " + quantity[i] + " items " + prices[i] + " rub/item, " + sum + " rub " + " totally");
+                totalSum += sum;
             }
         }
         System.out.println("Total: " + totalSum + " rub");
+    }
+
+    private static boolean isPromo(String[] promoProducts, String nameOfGood) {
+        for (String item : promoProducts) {
+            if (nameOfGood.equals(item)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
 
